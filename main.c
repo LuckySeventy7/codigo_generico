2921c71c441f03799b64f3bd9830713beb19353a
Sergio Ruiz <sergio.rruvalcaba@alumnos.udg.mx>
2020-09-28 00:49:34 -0500

    calcular area del circulo

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+int main() {
+   float a, r;
+ 
+   printf("Ingresa el radio del circulo: ");
+   scanf("%d", &r);
+ 
+   a = r * r *3.14;
+   printf("\nArea del circulo es : %f", area);
+ 
+   return (0);
+}
